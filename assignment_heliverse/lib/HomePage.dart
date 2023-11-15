236014fa08e87bd0b96ed1609e709e9c87a49458
singhrishabh93 <singhrishabh1670@gmal.com>
2023-11-15 22:09:42 +0530

    Implemented Dynamic Filter

--- assignment_heliverse/lib/HomePage.dart
+++ assignment_heliverse/lib/HomePage.dart
@@ -12,8 +12,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   FetchUser _userList = FetchUser();
+
+  final List<String> categories = ['Domain', 'Gender', 'Availability'];
+  List<String> selectedCategories = [];
   @override
   Widget build(BuildContext context) {
+    final filterProducts = _userList.data.where((product) {
+      return selectedCategories.isEmpty ||
+          selectedCategories.contains(product.category);
+    }).toList();
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
@@ -28,110 +35,144 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-      body: Container(
-        child: FutureBuilder<List<Userlist>>(
-            future: _userList.getUserList(),
-            builder: (context, snapshot) {
-              var data = snapshot.data;
-              if (!snapshot.hasData) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-                // print(data);
-              }
-              return ListView.builder(
-                  itemCount: _userList.data.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      child: Card(
-                        child: ListTile(
-                          title: Row(
-                            children: [
-                              Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                    // color: Colors.purple.shade700,
-                                    border: Border.all(
-                                        color: Colors.black, width: 2),
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Center(
-                                  child:
-                                      // Text("ID"),
-                                      Image.network("${data?[index].avatar}"),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: categories
+                .map((category) => FilterChip(
+                    label: Text(category),
+                    selected: selectedCategories.contains(category),
+                    onSelected: (selected) {
+                      setState(() {
+                        if (selected) {
+                          selectedCategories.add(category);
+                        } else {
+                          selectedCategories.remove(category);
+                        }
+                      });
+                    }))
+                .toList(),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 670,
+            child: FutureBuilder<List<Userlist>>(
+                future: _userList.getUserList(),
+                builder: (context, snapshot) {
+                  var data = snapshot.data;
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                    // print(data);
+                  }
+                  return ListView.builder(
+                      // itemCount: _userList.data.length,
+                      itemCount: filterProducts.length,
+                      itemBuilder: (context, index) {
+                        final product = filterProducts[index];
+                        return Container(
+                          child: Card(
+                            child: ListTile(
+                              title: Row(
                                 children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        "Name:",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      Text("${data?[index].firstName}"),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Text("${data?[index].lastName}")
-                                    ],
+                                  Container(
+                                    height: 50,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                        // color: Colors.purple.shade700,
+                                        border: Border.all(
+                                            color: Colors.black, width: 2),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: Center(
+                                      child:
+                                          // Text("ID"),
+                                          Image.network(
+                                              "${data?[index].avatar}"),
+                                    ),
                                   ),
-                                  Row(
-                                    children: [
-                                      Text(
-                                        "Email:",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      Text("${data?[index].email}"),
-                                    ],
+                                  const SizedBox(
+                                    width: 10,
                                   ),
-                                  Row(
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
-                                      Text(
-                                        "Gender:",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
+                                      Row(
+                                        children: [
+                                          const Text(
+                                            "Name:",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text("${data?[index].firstName}"),
+                                          const SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text("${data?[index].lastName}")
+                                        ],
                                       ),
-                                      SizedBox(
-                                        width: 5,
+                                      Row(
+                                        children: [
+                                          const Text(
+                                            "Email:",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text("${data?[index].email}"),
+                                        ],
                                       ),
-                                      Text("${data?[index].gender}"),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Text(
-                                        "Domain:",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
+                                      Row(
+                                        children: [
+                                          const Text(
+                                            "Gender:",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text("${data?[index].gender}"),
+                                        ],
                                       ),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      Text("${data?[index].domain}"),
+                                      Row(
+                                        children: [
+                                          const Text(
+                                            "Domain:",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          // Text("${data?[index].domain}"),
+                                          Text("${data?[index].domain}"),
+                                        ],
+                                      )
                                     ],
                                   )
                                 ],
-                              )
-                            ],
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    );
-                  });
-            }),
+                        );
+                      });
+                }),
+          ),
+        ],
       ),
     ));
   }
